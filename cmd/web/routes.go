fb055f39f4ef69c292bf55bae433c24fb0a632af
pasiemos <mariaamparo_thomas@gmx.de>
2021-12-07 16:05:03 +0100

    Adding images

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -26,6 +26,9 @@ mux.Use(SessionLoad)
 mux.Get("/", handlers.Repo.Home)
 mux.Get("/about", handlers.Repo.About)
 
+fileServer := http.FileServer(http.Dir("./static/"))
+mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
 return mux
 }
 
