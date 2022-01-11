503e6793fcba6b139af1b8ae49e53152bfe34e0e
pasiemos <mariaamparo_thomas@gmx.de>
2022-01-11 07:54:39 +0100

    updating

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -10,25 +10,31 @@ import (
 )
 
 func routes(app *config.AppConfig) http.Handler{
-//mux create a mulitplexer
+	//mux create a mulitplexer
 
-/*mux := pat.New()
+	/*mux := pat.New()
 
-mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-mux.Get("/about", http.HandlerFunc(handlers.Repo.About)) */
+	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	mux.Get("/about", http.HandlerFunc(handlers.Repo.About)) */
 
-mux := chi.NewRouter()
+	mux := chi.NewRouter()
 
-mux.Use(middleware.Recoverer)
-mux.Use(NoSurf)
-mux.Use(SessionLoad)
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
 
-mux.Get("/", handlers.Repo.Home)
-mux.Get("/about", handlers.Repo.About)
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/generals-quarters", handlers.Repo.Generals)
+	mux.Get("/majors-suite", handlers.Repo.Majors)
+	mux.Get("/search-availability", handlers.Repo.Availability)
+	mux.Get("/contact", handlers.Repo.Contact)
 
-fileServer := http.FileServer(http.Dir("./static/"))
-mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+	mux.Get("/make-reservation", handlers.Repo.Reservation)
 
-return mux
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
+	return mux
 }
 
